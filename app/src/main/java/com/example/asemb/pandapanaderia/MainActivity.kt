79dd813454ce02156850c1fcbb7bc34132bdab69
David Mejia <franklin.mejia@epn.edu.ec>
2019-01-16 18:02:58 -0500

    Agregado Interfaz Productos

--- app/src/main/java/com/example/asemb/pandapanaderia/MainActivity.kt
+++ app/src/main/java/com/example/asemb/pandapanaderia/MainActivity.kt
@@ -7,8 +7,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var menuinicio=0
 
     val manager = this.supportFragmentManager
+
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -34,9 +36,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (menuinicio ===0) {
+            createFragmenteHome()
+            menuinicio=1
+        }
+
         main_nav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        createFragmenteHome()
+        if(main_nav.getMenu().findItem(R.id.nav_home).isChecked()){
+            //finish();
+            moveTaskToBack(true);
+        }else{
+            main_nav.selectedItemId=R.id.nav_home
+        }
+    }
     fun createFragmenteHome()
     {
         val trans = manager.beginTransaction()
